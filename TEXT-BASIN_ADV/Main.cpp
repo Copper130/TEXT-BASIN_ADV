66c8bb2caddcfebe875f6d54acbbf03afdea82d0
Cooper Hampton <Cooper09h@gmail.com>
2025-11-18 10:10:25 -0700

    Just made choices make a bit more sense and restructered a small bit

--- TEXT-BASIN_ADV/Main.cpp
+++ TEXT-BASIN_ADV/Main.cpp
@@ -14,6 +14,11 @@
 
 using namespace std;
 
+void cls() {
+	system("cls");
+
+}
+
 // Clears Input Buffer as to provent infinite loops on invalid input
 void ClearInputBuffer() {
 	cin.clear();
@@ -31,12 +36,8 @@ int GetChoice(string prompt, int number_of_choices) {
 		cin >> choice;
 
 	} while (choice < 1 || choice > number_of_choices);
+	cls();
 	return choice;
-
-}
-
-void cls() {
-	system("cls");
 
 }
 
@@ -51,16 +52,16 @@ void TypeOut(const std::string& Text, int delayMs = 50) {
 }
 
 int typingspeed = 50; //Global variable for typing speed
-int main(){
-	
+int main() {
+
 	string Name;
 	std::cout << "Hello welcome to Venessa, a Text based adventure game! \n Please enter your name User." << std::endl;
-	
-	getline(cin,Name);
+
+	getline(cin, Name);
 
 	cls();
 	TypeOut("Welcome " + Name + ", to the world of Venessa! \n\n", 20);
-	
+
 	Sleep(1000); //Allow User to read welcome message
 
 	TypeOut("Your story starts as you awake with a splitting headache and little memory. As you look around you find yourself on a spaceship with dim lighting and an erie silence, as you look around you spot two diffrent hallways. The first appares to have a medical sign pointing to it, The other appares to be the direction to the cockpit of the ship\n\n", typingspeed);
@@ -68,8 +69,9 @@ int main(){
 
 	int first_choice = GetChoice("Which hallway do you choose? \n 1. Medical Bay \n 2. Cockpit", 2);
 	cls();
-
+	bool Went_to_medbay{};
 	if (first_choice == 1) {
+		 Went_to_medbay = true;
 		TypeOut("You head towards the medical bay, as you enter you see a few medical pods, one of them is open and appears to have some medical supplies inside. Do you want to take the supplies? \n\n", typingspeed);
 		int med_choice = GetChoice(" 1. Take them \n 2. Leave them", 2);
 		if (med_choice == 1) {
@@ -80,44 +82,63 @@ int main(){
 			TypeOut("You decide not to take the supplies despite having a crippling migrane and pain making it hard to think. \n\n", typingspeed);
 			bool TookMeds = false;
 		}
+
+
 	}
+	else {
+		 Went_to_medbay = false;
+	}
+		if (Went_to_medbay == true){
+			TypeOut("You head towards the cockpit, as the door is locked and you must figure out the code. as you look at the four digit lock you notice a clock next to it showing the current time. Whats your guess?\n\n", typingspeed);
 
-	TypeOut("You head towards the cockpit, as the door is locked and you must figure out the code. as you look at the four digit lock you notice a clock next to it showing the current time. Whats your guess?\n\n", typingspeed);
-	
-	int timeCode = 1000;
-	int code_guess = 12;
-	 do {
-		int code_guess = 6000;
-		std::cout << "Enter the 4-digit code: ";
-		ClearInputBuffer();
-		cin >> code_guess;
-		
-	// Get the current time
-	time_t now = time(0);
-	tm* localTime = localtime(&now);
+		int timeCode = 1000;
+		int code_guess = 12;
+		while (code_guess != timeCode) {
+			int code_guess = 6000;
+			std::cout << "Enter the 4-digit code: ";
+			ClearInputBuffer();
+			cin >> code_guess;
 
-	int hour = localTime->tm_hour;
-	int minute = localTime->tm_min;
+			// Get the current time
+			time_t now = time(0);
+			tm* localTime = localtime(&now);
 
-	// Combine into HHMM format (e.g., 1437 for 2:37 PM)
-	int timeCode = hour * 100 + minute;
-	//std::cout << "Debug: The correct code is " << timeCode << std::endl;
-	
-		
-		if (code_guess == timeCode) {
-			cls();
-			break;
+			int hour = localTime->tm_hour;
+			int minute = localTime->tm_min;
+
+			// Combine into HHMM format (e.g., 1437 for 2:37 PM)
+			int timeCode = hour * 100 + minute;
+			//std::cout << "Debug: The correct code is " << timeCode << std::endl;
+
+
+			if (code_guess == timeCode) {
+				cls();
+				break;
+			}
+			TypeOut("The lock lets out a buzzing sound and a faint red light. Try again.\n", 40);
 		}
-		TypeOut("The lock lets out a buzzing sound and a faint red light. Try again.\n", 40);
-	}while (code_guess != timeCode);
-	TypeOut("The lock beeps and the door slides open, you enter the cockpit and find the controls of the ship. Suddenly, an alarm goes off indicating that the ship's systems are failing, the ship was constructed to be operated by a team of people 24/7 due to the lack of people operating and upkeeping systems they are failing!\n\n", typingspeed);
+
+		TypeOut("The lock beeps and the door slides open, you enter the cockpit and find the controls of the ship. Suddenly, an alarm goes off indicating that the ship's systems are failing, the ship was constructed to be operated by a team of people 24/7 due to the lack of people operating and upkeeping systems they are failing!\n\n", typingspeed);
+		}
+
 	Sleep(1000);
 	cls();
-	for (int i = 8; i > 0; i--) {
-		system("color 4");//Set text color to red
-		TypeOut("You see a planet getting closer.\n", 3);
-		TypeOut("Impact in " + to_string(i) + " seconds!\n", 3);
+	int i = 8;
+	if (Went_to_medbay == false) {
+		for (i; i > 0; i--) {
+			system("color 4");//Set text color to red
+			TypeOut("You see a planet getting closer.\n", 3);
+			TypeOut("Impact in " + to_string(i) + " seconds!\n", 3);
+		}
 	}
+	else {
+		for (i; i > 0; i--) {
+			system("color 4");//Set text color to red
+			TypeOut("You are thrown around as the ship barrels to the ground.\n", 3);
+			TypeOut("Impact in " + to_string(i) + " seconds!\n", 3);
+		}
+	}
+
 	cls();
 	int IncreaseAmt = 1;
 	int fontSize = 16;
@@ -199,14 +220,14 @@ int main(){
 	};
 
 	int frameCount = sizeof(frames) / sizeof(frames[0]);
-	int i = 0;
+	int CurrentFrame = 0;
 	while (true) {
 		cls();
 		std::cout << "You have crash landed on an unknown planet...\n\n";
-		std::cout << frames[i] <<endl;
+		std::cout << frames[CurrentFrame] <<endl;
 		std::cout << "You are stuck." << endl;
-		//this_thread::sleep_for(chrono::milliseconds(200));
-		i = (i + 1) % frameCount;
+		this_thread::sleep_for(chrono::milliseconds(200));
+		CurrentFrame = (CurrentFrame + 1) % frameCount;
 	}
 
 	return 69; 
